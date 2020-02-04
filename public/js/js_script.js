5e0bbb4629a058d0d82a3cacf94374883c55ccdd
sebastianfallman <sebastian.fallman@live.com>
2020-02-04 19:05:07 +0100

    Fast

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,48 +1,26 @@
-/*function menuItem(name, allergies, kcal, pathToImg){
-    this.name = name;
-    this.allergies = 'Contains ' + allergies;
-    this.kcal =  kcal;
-    this.pathToImg = pathToImg;
-    this.burger = function() {
-        return this.name + ' ' + this.kcal  + ' kcal';
-    };
+let burgerMenu = document.getElementById("menu");
+
+for (let i = 0; i < food.length; ++i){
+    let burger = document.createElement("div");
+    let ul = document.createElement("ul");
+    let img = document.createElement("img");
+    img.src = food[i].pathToImg;
+    img.className = "burgerimg";
+
+    let burgerName = document.createTextNode(food[i].name);
+
+    let burgerKcal = document.createElement("li");
+    burgerKcal.appendChild(document.createTextNode(food[i].kcal + ' kcal'));
+
+    if (food[i].allergies !== ''){
+        let burgerAllergies = document.createElement("li");
+        burgerAllergies.appendChild(document.createTextNode('Contains ' + food[i].allergies));
+        ul.appendChild(burgerAllergies);
+    }
+
+    ul.appendChild(burgerKcal);
+    burger.appendChild(burgerName);
+    burger.appendChild(img);
+    burger.appendChild(ul)
+    burgerMenu.appendChild(burger);
 }
-
-
-let TheOG = new menuItem('The OG', 'gluten', 700, 'img/TheOG.png');
-
-let HolyCow = new menuItem('Holy Cow', 'lactose', 900, 'img/HolyCow.png')
-
-let TMC = new menuItem('The Monster Chickenburger', 'everything', 1500, 'img/TMC.png')
-
-let DoubleM = new menuItem('The Double', 'gluten', 800, '')
-
-let SingleM = new menuItem('The Single', 'gluten', 450, '')
-
-let element = document.getElementById("myID")
-
-let para1 = document.createElement("p");
-let para2 = document.createElement("p);
-let para3 = document.createElement("p");
-let para4 = document.createElement("p");
-let para5 = document.createElement("p");
-
-let node1 = document.createTextNode(TheOG.burger())
-let node2 = document.createTextNode(HolyCow.burger())
-let node3 = document.createTextNode(TMC.burger())
-let node4 = document.createTextNode(DoubleM.burger())
-let node5 = document.createTextNode(SingleM.burger())
-
-para1.appendChild(node1);
-para2.appendChild(node2);
-para3.appendChild(node3);
-para4.appendChild(node4);
-para5.appendChild(node5);
-
-element.appendChild(para1);
-element.appendChild(para2);
-element.appendChild(para3);
-element.appendChild(para4);
-element.appendChild(para5);
-
-// console.log(TheOG.burger() + '\n' + HolyCow.burger() + '\n' + TMC.burger() + '\n' + DoubleM.burger() + '\n' + SingleM.burger());*/
---
+++ public/js/menu.js
@@ -0,0 +1,36 @@
+let food = [
+    {
+        "name": "The OG",
+        "allergies": "gluten",
+        "kcal": 700,
+        "pathToImg": "img/TheOG.png"
+    },
+
+    {
+        "name": "Holy Cow",
+        "allergies": "lactose",
+        "kcal": 900,
+        "pathToImg": "img/HolyCow.png"
+    },
+
+    {
+        "name": "The Monster Chickenburger",
+        "allergies": "everything",
+        "kcal": 1500,
+        "pathToImg": "img/TMC.png"
+    },
+
+    {
+        "name": "The Double",
+        "allergies": "",
+        "kcal": 800,
+        "pathToImg": "https://www.burgerking.se/011_se/Product%20images/Burgers/image-thumb__16401__product_detail/Dubbel_Cheese_Produktbilder-Hemsida.png"
+    },
+
+    {
+        "name": "The Single",
+        "allergies": "gluten",
+        "kcal": 400,
+        "pathToImg": "https://www.burgerking.se/011_se/Product%20images/Burgers/image-thumb__15089__product_detail/Sourdough-Singlebeef_Produktbilder-Hemsida.png"
+    }
+];
