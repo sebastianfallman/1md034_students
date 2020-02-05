cd751978f96e9c7714e72a83f98354b34112db57
sebastianfallman <sebastian.fallman@live.com>
2020-02-05 17:38:51 +0100

    Task 4-10 klar

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -24,4 +24,34 @@ for (let i = 0; i < food.length; ++i){
     burger.appendChild(ul)
     burgerMenu.appendChild(burger);
 }
+
+
+// Collects the customer information
+function collectForm() { 
+    console.log("Tjena");
+    var myArray = [];
+    var myList = document.getElementById('payment');
+    myArray[0] = document.getElementById('fn').value;
+    myArray[1] = document.getElementById('email').value;
+    myArray[2] = document.getElementById('street').value;
+    myArray[3] = document.getElementById('hn').value;
+    myArray[4] = myList.options[myList.selectedIndex].text;
+    let gender = document.getElementsByName('gender');
+    for(var i = 0; i < gender.length; ++i){
+        if(gender[i].checked){
+            myArray[5] = gender[i].value;
+        }
+    }
+    console.log(myArray);
+}
+
+// Prints to console
+function buttonPressed() {
+    console.log("Button pressed");
+}
+
+let myButton = document.getElementById('orderButton');
+
+myButton.addEventListener("click", buttonPressed);
+myButton.addEventListener("click", collectForm);
 */
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,32 @@
 const vm = new Vue({
-    el: '#burgers',
-    data: { 
-      food: food
-    }
+  el: 'main',
+  data: {
+    orderedFood: [],
+    food: food,
+    finalOrder: ''
+  },
+  methods: {
+    markDone: function() {
+      
+      var myArray = [];
+      var myList = document.getElementById('payment');
+        
+      myArray[0] = document.getElementById('fn').value;
+      myArray[1] = document.getElementById('email').value;
+      myArray[2] = document.getElementById('street').value;
+      myArray[3] = document.getElementById('hn').value;
+      myArray[4] = myList.options[myList.selectedIndex].text;
+      
+      let gender = document.getElementsByName('gender');
+        
+      for(var i = 0; i < gender.length; ++i){
+          if(gender[i].checked){
+              myArray[5] = gender[i].value;
+          }
+      }
+  
+      myArray[6] = this.orderedFood.toString();
+      this.finalOrder = myArray;
+      }
+    }  
 });
